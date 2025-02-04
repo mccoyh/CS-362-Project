b71561ebec6c965bf85ac106bb5506e17dd63258
Alex Martin <sharkfinpro@hotmail.com>
2025-02-04 12:05:30 -0800

    Create instance

--- vulkanEngine/source/components/Instance.cpp
+++ vulkanEngine/source/components/Instance.cpp
@@ -1,4 +1,107 @@
 #include "Instance.h"
+#include <stdexcept>
+#include "DebugMessenger.h"
+#include <GLFW/glfw3.h>
+
+#ifdef NDEBUG
+constexpr bool enableValidationLayers = false;
+#else
+constexpr bool enableValidationLayers = true;
+#endif
 
 namespace vkEngine {
+  Instance::Instance()
+  {
+    if (enableValidationLayers && !checkValidationLayerSupport())
+    {
+      throw std::runtime_error("validation layers requested, but not available!");
+    }
+
+    constexpr VkApplicationInfo appInfo {
+      .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+      .pApplicationName = "Vulkan Engine",
+      .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+      .pEngineName = "No Engine",
+      .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+      .apiVersion = VK_API_VERSION_1_0
+    };
+
+    const auto extensions = getRequiredExtensions();
+
+    VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+    if (enableValidationLayers)
+    {
+      DebugMessenger::populateCreateInfo(debugCreateInfo);
+    }
+
+    const VkInstanceCreateInfo createInfo {
+      .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+      .pNext = enableValidationLayers ? &debugCreateInfo : nullptr,
+      .pApplicationInfo = &appInfo,
+      .enabledLayerCount = enableValidationLayers ? static_cast<uint32_t>(validationLayers.size()) : 0,
+      .ppEnabledLayerNames = enableValidationLayers ? validationLayers.data() : nullptr,
+      .enabledExtensionCount = static_cast<uint32_t>(extensions.size()),
+      .ppEnabledExtensionNames = extensions.data()
+    };
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+    {
+      throw std::runtime_error("failed to create instance!");
+    }
+  }
+
+  Instance::~Instance()
+  {
+    vkDestroyInstance(instance, nullptr);
+  }
+
+  VkInstance& Instance::getInstance()
+  {
+    return instance;
+  }
+
+  bool Instance::checkValidationLayerSupport()
+  {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for (const char* layerName : validationLayers)
+    {
+      bool layerFound = false;
+
+      for (const auto& layerProperties : availableLayers)
+      {
+        if (strcmp(layerName, layerProperties.layerName) == 0)
+        {
+          layerFound = true;
+          break;
+        }
+      }
+
+      if (!layerFound)
+      {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  std::vector<const char *> Instance::getRequiredExtensions()
+  {
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    std::vector extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+    if (enableValidationLayers)
+    {
+      extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+
+    return extensions;
+  }
 } // vkEngine
--- vulkanEngine/source/components/Instance.h
+++ vulkanEngine/source/components/Instance.h
@@ -1,10 +1,29 @@
 #ifndef INSTANCE_H
 #define INSTANCE_H
 
+#include <vulkan/vulkan.h>
+#include <vector>
+#include <array>
+
 namespace vkEngine {
 
-class Instance {
+constexpr std::array<const char*, 1> validationLayers {
+  "VK_LAYER_KHRONOS_validation"
+};
 
+class Instance {
+public:
+  Instance();
+  ~Instance();
+
+  VkInstance& getInstance();
+
+private:
+  VkInstance instance{};
+
+  static bool checkValidationLayerSupport();
+
+  static std::vector<const char*> getRequiredExtensions();
 };
 
 } // vkEngine
