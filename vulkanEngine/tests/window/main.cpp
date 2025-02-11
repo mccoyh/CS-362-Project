4c5b7afa6d9104f3e4b45e3dc081a0cfe02914b3
Alex Martin <sharkfinpro@hotmail.com>
2025-02-11 14:56:17 -0800

    Catch errors in test

--- vulkanEngine/tests/window/main.cpp
+++ vulkanEngine/tests/window/main.cpp
@@ -1,17 +1,28 @@
 #include <source/VulkanEngine.h>
+#include <iostream>
 
 int main()
 {
-  constexpr VkEngine::VulkanEngineOptions vulkanEngineOptions {
-    .WINDOW_WIDTH = 600,
-    .WINDOW_HEIGHT = 400,
-    .WINDOW_TITLE = "Window Test"
-  };
-
-  auto vulkanEngine = VkEngine::VulkanEngine(vulkanEngineOptions);
-
-  while (vulkanEngine.isActive())
+  try
   {
-    vulkanEngine.render();
+    constexpr VkEngine::VulkanEngineOptions vulkanEngineOptions {
+      .WINDOW_WIDTH = 600,
+      .WINDOW_HEIGHT = 400,
+      .WINDOW_TITLE = "Window Test"
+    };
+
+    auto vulkanEngine = VkEngine::VulkanEngine(vulkanEngineOptions);
+
+    while (vulkanEngine.isActive())
+    {
+      vulkanEngine.render();
+    }
   }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
