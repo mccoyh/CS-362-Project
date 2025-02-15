7ba4b868ad7987977ff44a2a2bf3b40fa351572e
Alex Martin <sharkfinpro@hotmail.com>
2025-02-14 21:57:34 -0800

    fix compiling issues

--- audiolib/source/audio.cpp
+++ audiolib/source/audio.cpp
@@ -1,9 +1,17 @@
 #include "audio.h"
+#include <stdexcept>
+#include <SDL3/SDL_audio.h>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+}
 
 //checks if file exists
-inline bool exists (const std::string& name) {
-    struct stat buffer;   
-    return (stat (name.c_str(), &buffer) == 0); 
+bool exists (const std::string& name) {
+    struct stat buffer;
+    return (stat (name.c_str(), &buffer) == 0);
 }
 
 //converts mp4 to mp3, pulls audio from video
@@ -11,39 +19,37 @@ void convert_mp4_mp3(std::string input_mp4){
     if(exists(input_mp4)){
         //checks for any existing output.mp3 files that already exist
         int num = 0;
-        while(exists("output" + num + ".mp3")){
-            num++
+        while(exists("output" + std::to_string(num) + ".mp3")){
+            num++;
         }
-        system("ffmpeg -i " + input_mp4 + " output" + num + ".mp3")
+        system(("ffmpeg -i " + input_mp4 + " output" + std::to_string(num) + ".mp3").c_str());
     }
 }
 
 void play_audio(std::string input_mp3){
-    av_register_all();
-
     AVFormatContext *formatContext = avformat_alloc_context();
-    if (avformat_open_input(&formatContext, input_mp3, NULL, NULL) != 0) {
+    if (avformat_open_input(&formatContext, input_mp3.c_str(), NULL, NULL) != 0) {
         throw std::runtime_error("failed to open input file");
     }
 
     int audioStreamIndex = -1;
-    for (int i = 0; i < formatContext->nb_streams; i++) {
+    for (unsigned int i = 0; i < formatContext->nb_streams; i++) {
         if (formatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
-            audioStreamIndex = i;
+            audioStreamIndex = static_cast<int>(i);
             break;
         }
     }
 
     AVCodecContext *codecContext = avcodec_alloc_context3(NULL);
     avcodec_parameters_to_context(codecContext, formatContext->streams[audioStreamIndex]->codecpar);
-    AVCodec *codec = avcodec_find_decoder(codecContext->codec_id);
+    const AVCodec *codec = avcodec_find_decoder(codecContext->codec_id);
     avcodec_open2(codecContext, codec, NULL);
 
-    SDL_AudioSpec desiredSpec, obtainedSpec;
+    SDL_AudioSpec desiredSpec;
     desiredSpec.freq = codecContext->sample_rate;
-    desiredSpec.format = AUDIO_S16SYS;
-    desiredSpec.channels = codecContext->channels;
-    desiredSpec.samples = 1024;
-    SDL_AudioDeviceID audioDevice = SDL_OpenAudioDevice(NULL, 0, &desiredSpec, &obtainedSpec, 0);
-    SDL_PauseAudioDevice(audioDevice, 0); // Start playback
+    // desiredSpec.format = AUDIO_S16SYS;
+    // desiredSpec.channels = codecContext->channels;
+    // desiredSpec.samples = 1024;
+    SDL_AudioDeviceID audioDevice = SDL_OpenAudioDevice(0, &desiredSpec);
+    SDL_PauseAudioDevice(0); // Start playback
 }
