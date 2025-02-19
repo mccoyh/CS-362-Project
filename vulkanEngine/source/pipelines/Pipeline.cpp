f9d47ba823bfcd4d39f24ea91455e3dd27a29d8a
Alex Martin <sharkfinpro@hotmail.com>
2025-02-18 17:42:58 -0800

    Create Pipeline

---
+++ vulkanEngine/source/pipelines/Pipeline.cpp
@@ -0,0 +1,34 @@
+#include "Pipeline.h"
+#include "../components/LogicalDevice.h"
+#include "../components/PhysicalDevice.h"
+#include <iostream>
+
+namespace VkEngine {
+  Pipeline::Pipeline(const std::shared_ptr<PhysicalDevice>& physicalDevice,
+                     const std::shared_ptr<LogicalDevice>& logicalDevice)
+    : physicalDevice(physicalDevice), logicalDevice(logicalDevice)
+  {}
+
+  Pipeline::~Pipeline()
+  {
+    if (pipeline != VK_NULL_HANDLE)
+    {
+      vkDestroyPipeline(logicalDevice->getDevice(), pipeline, nullptr);
+      pipeline = VK_NULL_HANDLE;
+    }
+    else
+    {
+      std::cerr << "Warning: Attempted to destroy an invalid pipeline!" << std::endl;
+    }
+
+    if (pipelineLayout != VK_NULL_HANDLE)
+    {
+      vkDestroyPipelineLayout(logicalDevice->getDevice(), pipelineLayout, nullptr);
+      pipelineLayout = VK_NULL_HANDLE;
+    }
+    else
+    {
+      std::cerr << "Warning: Attempted to destroy an invalid pipeline layout!" << std::endl;
+    }
+  }
+} // VkEngine
