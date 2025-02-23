0a55f658c8260d2cdfe7ddb52268bef0a7418248
Alex Martin <sharkfinpro@hotmail.com>
2025-02-23 15:56:47 -0800

    create new test

---
+++ vulkanEngine/tests/videoDecode/main.cpp
@@ -0,0 +1,28 @@
+#include <source/VulkanEngine.h>
+#include <iostream>
+
+int main()
+{
+  try
+  {
+    constexpr VkEngine::VulkanEngineOptions vulkanEngineOptions {
+      .WINDOW_WIDTH = 600,
+      .WINDOW_HEIGHT = 400,
+      .WINDOW_TITLE = "Video Decoding"
+    };
+
+    auto vulkanEngine = VkEngine::VulkanEngine(vulkanEngineOptions);
+
+    while (vulkanEngine.isActive())
+    {
+      vulkanEngine.render();
+    }
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
