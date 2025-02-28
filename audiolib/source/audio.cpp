6bd787024725263f59c468d1cfb2c74e568ced05
mccoyh <mccoyh@oregonstate.edu>
2025-02-27 17:10:36 -0800

    bad patch job
    found a fix for audio not building, placed all sdl header files into audiolib.

--- audiolib/source/audio.cpp
+++ audiolib/source/audio.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <stdexcept>
 #include <filesystem>
-#include <SDL3/SDL_audio.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_timer.h>
 
@@ -33,44 +32,77 @@ namespace Audio {
         }
     }
 
-    bool playAudio(const char* input_wav) {   
+    AudioData playAudio(const char* input_wav) {   
         //initizalize SDL
         SDL_Init(SDL_INIT_AUDIO);
 
-        SDL_AudioSpec spec;
+        //SDL_AudioSpec spec;
 
         uint8_t *audio; //audio data
         uint32_t len; //audio data length, not duration of audio file
+        AudioData ad;
 
         //load wav file
-        if (SDL_LoadWAV(input_wav, &spec, &audio, &len) == NULL){
+        if (SDL_LoadWAV(input_wav, &ad.spec, &audio, &len) == NULL){
             std::cout << "Error loading file" << std::endl;
-            return false;
+            return ad;
         }
 
-        SDL_AudioStream *stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec, NULL, NULL);
-        SDL_PutAudioStreamData(stream, audio, sizeof(uint8_t)*len);
-        SDL_ResumeAudioStreamDevice(stream); // Start playback
+        ad.stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &ad.spec, NULL, NULL);
+        //ad.spec = spec;
+        SDL_PutAudioStreamData(ad.stream, audio, sizeof(uint8_t)*len);
+        SDL_ResumeAudioStreamDevice(ad.stream); // Start playback
         
         //Duration of wav file in milliseconds
-        uint32_t duration = ((((len/(SDL_AUDIO_BITSIZE(spec.format)/8)) / spec.channels) / spec.freq) * 1000) + 150; // extra 150 ms for buffer
+        ad.duration = ((((len/(SDL_AUDIO_BITSIZE(ad.spec.format)/8)) / ad.spec.channels) / ad.spec.freq) * 1000) + 150; // extra 150 ms for buffer
 
-        while(duration > 0){
-            SDL_Delay(10); // check for more inputs every 10 ms
+        // while(duration > 0){
+        //     SDL_Delay(10); // check for more inputs every 10 ms
 
-            // add any input checks
+        //     // add any input checks
 
-            duration -= 10;
-        }
+        //     duration -= 10;
+        // }
 
         // Cleanup
+        // SDL_ClearAudioStream(stream);
+        // SDL_DestroyAudioStream(stream);
+        SDL_free(audio);
+        // SDL_QuitSubSystem(SDL_INIT_AUDIO);
+        // SDL_QuitSubSystem(SDL_INIT_EVENTS);
+        // SDL_Quit();
+
+        return ad;
+    }
+
+    void initSDL(){
+        SDL_Init(SDL_INIT_AUDIO);
+    }
+    
+    void delay(uint32_t ms){
+        SDL_Delay(ms);
+    }
+    
+    void deleteStream(SDL_AudioStream *stream){
         SDL_ClearAudioStream(stream);
         SDL_DestroyAudioStream(stream);
-        SDL_free(audio);
+    }
+    
+    void quitSDL(){
         SDL_QuitSubSystem(SDL_INIT_AUDIO);
         SDL_QuitSubSystem(SDL_INIT_EVENTS);
         SDL_Quit();
+    }
 
-        return true;
+    void pauseAudio(SDL_AudioStream* stream){
+        if (SDL_AudioStreamDevicePaused(stream)){
+            SDL_PauseAudioStreamDevice(stream);
+        } else {
+            SDL_ResumeAudioStreamDevice(stream);
+        }
+    }
+
+    bool isPaused(SDL_AudioStream* stream){
+        return SDL_AudioStreamDevicePaused(stream);
     }
 } // namespace Audio
--- audiolib/tests/audioplayback/main.cpp
+++ audiolib/tests/audioplayback/main.cpp
@@ -1,6 +1,23 @@
 #include <source/audio.h>
 
+//using namespace Audio;
+
 int main() 
 {
-  Audio::playAudio("audio.wav");
+  Audio::initSDL();
+
+  // create and play audio stream
+  Audio::AudioData audio = Audio::playAudio("audio.wav");
+  uint32_t duration = audio.duration;
+
+  while(duration > 0){
+    Audio::delay(10); // checks for extra input every 10 ms
+    
+    // do any checks for other inputs
+    
+    duration -= 10;
+  }
+
+  Audio::deleteStream(audio.stream);
+  Audio::quitSDL();
 }
