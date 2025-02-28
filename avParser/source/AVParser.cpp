db58ca10914fc378e603b7d943f545b39163466e
Alex Martin <sharkfinpro@hotmail.com>
2025-02-28 11:33:59 -0800

    create initial class

---
+++ avParser/source/AVParser.cpp
@@ -0,0 +1,4 @@
+#include "AVParser.h"
+
+namespace AVParser {
+} // AVParser
---
+++ avParser/source/AVParser.h
@@ -0,0 +1,12 @@
+#ifndef AVPARSER_H
+#define AVPARSER_H
+
+namespace AVParser {
+
+class AVParser {
+
+};
+
+} // AVParser
+
+#endif //AVPARSER_H
