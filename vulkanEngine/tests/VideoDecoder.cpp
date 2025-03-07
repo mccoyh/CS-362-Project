3b5c229a532d8fb9fbf84eabe625cad3b4d1a431
Alex Martin <sharkfinpro@hotmail.com>
2025-03-06 18:31:44 -0800

    add swresample header

--- vulkanEngine/tests/VideoDecoder.cpp
+++ vulkanEngine/tests/VideoDecoder.cpp
@@ -5,6 +5,7 @@ extern "C" {
 #include <libavformat/avformat.h>
 #include <libavcodec/avcodec.h>
 #include <libswscale/swscale.h>
+#include <libswresample/swresample.h>
 }
 
 VideoDecoder::VideoDecoder(const char *filename)
