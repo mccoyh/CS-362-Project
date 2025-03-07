aefc0a5bfc009267af208a662c9db9efd5e6d931
Alex Martin <sharkfinpro@hotmail.com>
2025-03-07 12:16:05 -0800

    cleanup

--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,12 @@
 #include <filesystem>
 #include "../AudioToTxt/tests/test_whisper/audioDecoding.h"
 
+constexpr VkEngine::VulkanEngineOptions vulkanEngineOptions {
+  .WINDOW_WIDTH = 1000,
+  .WINDOW_HEIGHT = 600,
+  .WINDOW_TITLE = "Medos Media Player"
+};
+
 int main(const int argc, char* argv[])
 {
   try
@@ -15,29 +21,30 @@ int main(const int argc, char* argv[])
 
     /* begin captions initialization */
 
-    std::filesystem::path exePath = std::filesystem::current_path(); // Gets the current working directory (where executable runs)
+    // Path to the assets folder as a string
+    std::string assetsPath = "assets/";
 
-    // Path to the assets folder relative to the executable's directory
-    std::filesystem::path assetsPath =  exePath / "assets";
+    const std::string audioFile = assetsPath + "audio.pcm";
+    const std::string subtitleFile = assetsPath + "subtitles.srt";
+    const std::string outputVideo = assetsPath + "output_with_subtitles_turbo.mp4";
+    const std::string modelPath = "models/ggml-large-v3-turbo-q5_0.bin"; // "ggml-base.bin"
 
-    const std::filesystem::path audioFile = assetsPath / "audio.pcm";
-    const std::filesystem::path subtitleFile = assetsPath / "subtitles.srt";
-    const std::filesystem::path outputVideo = assetsPath / "output_with_subtitles_turbo.mp4";
-    const std::filesystem::path modelPath = exePath / "models" / "ggml-large-v3-turbo-q5_0.bin"; //"ggml-base.bin"
-
-    std::cout <<"pwd: " << exePath << std::endl
-        << "Assests path: " << assetsPath << std::endl
+    std::cout << "Assests path: " << assetsPath << std::endl
         << "Model path: "<< modelPath << std::endl
-        <<  "Input file: " << asset << std::endl
+        << "Input file: " << asset << std::endl
         << "subtitle file: " << subtitleFile << std::endl;
-    if (!extractAudio(asset, audioFile.string())){
+
+    if (!extractAudio(asset, audioFile))
+    {
       std::cout << "Failed to generate formated audio file" << std::endl;
     }
-    if (Captions::transcribeAudio(modelPath.string(), audioFile.string(), subtitleFile.string()) != 0){
+
+    if (Captions::transcribeAudio(modelPath, audioFile, subtitleFile) != 0)
+    {
       std::cout << "Failed to generate subtitles" << std::endl;
     }
 
-    Captions::CaptionCache cache(subtitleFile.string());
+    Captions::CaptionCache cache(subtitleFile);
 
     /* End captions initialization */
 
@@ -64,12 +71,6 @@ int main(const int argc, char* argv[])
     std::cout << "Frame Width: " << frameData.frameWidth << "\n"
               << "Frame Height: " << frameData.frameHeight << "\n"
               << "Total Frames: " << parser.getTotalFrames() << std::endl;
-
-    constexpr VkEngine::VulkanEngineOptions vulkanEngineOptions {
-      .WINDOW_WIDTH = 600,
-      .WINDOW_HEIGHT = 400,
-      .WINDOW_TITLE = "Video Decoding"
-    };
 
     auto vulkanEngine = VkEngine::VulkanEngine(vulkanEngineOptions);
     ImGui::SetCurrentContext(VkEngine::VulkanEngine::getImGuiContext());
